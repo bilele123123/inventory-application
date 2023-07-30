91dd7e57d4c8d11075e998892da75f9e94addfb4
bilele123123 <tple06203@gmail.com>
2023-07-29 23:59:40 -0700

    Add category function and update webpage view.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,3 +1,18 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
+// Display list of all Categories.
+exports.category_list = asayncHandler(async (req, res, next) => {
+    
+})
+
+// Display detail page for a specific Category.
+exports.category_detail = asyncHandler(async (req, res, next) => {
+    // Get details of categories of products.
+})
+
+
+// Display Category create form on GET. 
+exports.category_create_get = (req, res, next) => {
+    res.render("", { title: "Create Category"});
+;}
