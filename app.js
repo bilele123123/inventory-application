67144d0ba69faac88753b80d9a99d4eeaa19a75e
bilele123123 <tple06203@gmail.com>
2023-07-30 23:55:26 -0700

    Connect and secured mongoose connection.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,6 +9,17 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
