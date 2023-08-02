2bb28bb906cd058141d2d7c9e3c88ffacae355cb
bilele123123 <tple06203@gmail.com>
2023-08-01 23:03:40 -0700

    Set up basic controller function.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,18 +1,43 @@
 const { body, validationResult } = require("express-validator");
+const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Categories.
-exports.category_list = asayncHandler(async (req, res, next) => {
-    
-})
+exports.category_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category list");
+});
 
-// Display detail page for a specific Category.
+// Display detail page for a specific category.
 exports.category_detail = asyncHandler(async (req, res, next) => {
-    // Get details of categories of products.
-})
+  res.send(`NOT IMPLEMENTED: category detail: ${req.params.id}`);
+});
 
+// Display category create form on GET.
+exports.category_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category create GET");
+});
 
-// Display Category create form on GET. 
-exports.category_create_get = (req, res, next) => {
-    res.render("", { title: "Create Category"});
-;}
+// Handle category create on POST.
+exports.category_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category create POST");
+});
+
+// Display category delete form on GET.
+exports.category_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category delete GET");
+});
+
+// Handle category delete on POST.
+exports.category_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category delete POST");
+});
+
+// Display category update form on GET.
+exports.category_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category update GET");
+});
+
+// Handle category update on POST.
+exports.category_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: category update POST");
+});
