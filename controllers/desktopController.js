ab9ef3c60d81017b7cab9289b4b7ea8fd4b5e40c
bilele123123 <tple06203@gmail.com>
2023-08-01 23:10:34 -0700

    Update placeholder function for controllers.

---
+++ controllers/desktopController.js
@@ -0,0 +1,43 @@
+const { body, validationResult } = require("express-validator");
+const Desktop = require("../models/desktop");
+const asyncHandler = require("express-async-handler");
+
+// Display list of all Categories.
+exports.desktop_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: desktop list");
+});
+
+// Display detail page for a specific desktop.
+exports.desktop_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: desktop detail: ${req.params.id}`);
+});
+
+// Display desktop create form on GET.
+exports.desktop_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: desktop create GET");
+});
+
+// Handle desktop create on POST.
+exports.desktop_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: desktop create POST");
+});
+
+// Display desktop delete form on GET.
+exports.desktop_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: desktop delete GET");
+});
+
+// Handle desktop delete on POST.
+exports.desktop_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: desktop delete POST");
+});
+
+// Display desktop update form on GET.
+exports.desktop_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: desktop update GET");
+});
+
+// Handle desktop update on POST.
+exports.desktop_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: desktop update POST");
+});
---
+++ controllers/laptopController.js
@@ -0,0 +1,43 @@
+const { body, validationResult } = require("express-validator");
+const Laptop = require("../models/laptop");
+const asyncHandler = require("express-async-handler");
+
+// Display list of all Categories.
+exports.laptop_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: laptop list");
+});
+
+// Display detail page for a specific laptop.
+exports.laptop_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: laptop detail: ${req.params.id}`);
+});
+
+// Display laptop create form on GET.
+exports.laptop_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: laptop create GET");
+});
+
+// Handle laptop create on POST.
+exports.laptop_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: laptop create POST");
+});
+
+// Display laptop delete form on GET.
+exports.laptop_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: laptop delete GET");
+});
+
+// Handle laptop delete on POST.
+exports.laptop_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: laptop delete POST");
+});
+
+// Display laptop update form on GET.
+exports.laptop_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: laptop update GET");
+});
+
+// Handle laptop update on POST.
+exports.laptop_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: laptop update POST");
+});
