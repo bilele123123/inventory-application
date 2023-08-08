a62b29b46e05c3a7df08ad44420ce32cc715690b
bilele123123 <tple06203@gmail.com>
2023-08-07 22:03:29 -0700

    Add display create new desktop form and view.

--- controllers/desktopController.js
+++ controllers/desktopController.js
@@ -38,16 +38,64 @@ exports.desktop_detail = asyncHandler(async (req, res, next) => {
   }
 });
 
-
 // Display desktop create form on GET.
 exports.desktop_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: desktop create GET");
+  res.render("desktop_form", { title: "Create New Desktop" });
 });
 
-// Handle desktop create on POST.
-exports.desktop_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: desktop create POST");
-});
+// Handle Desktop create on POST.
+// Handle Desktop create on POST.
+exports.desktop_create_post = [
+  // Validate and sanitize the name, brand, model, description, price, and numberInStock fields.
+  body("name", "Desktop name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("brand", "Enter a valid brand").trim().notEmpty().escape(),
+  body("model", "Enter a valid model").trim().notEmpty().escape(),
+  body("description", "Enter a valid description").trim().notEmpty().escape(),
+  body("price", "Enter a valid price").isFloat({ min: 0 }),
+  body("numberInStock", "Enter a valid number in stock").isInt({ min: 0 }),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a desktop object with escaped and trimmed data.
+    const desktop = new Desktop({
+      name: req.body.name,
+      brand: req.body.brand,
+      model: req.body.model,
+      description: req.body.description,
+      price: req.body.price,
+      numberInStock: req.body.numberInStock,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("desktop_form", {
+        title: "Create New Desktop",
+        desktop: desktop,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Desktop with same name already exists.
+      const desktopExists = await Desktop.findOne({ name: req.body.name }).exec();
+      if (desktopExists) {
+        // Desktop exists, redirect to its detail page.
+        res.redirect(desktopExists.url);
+      } else {
+        await desktop.save();
+        // New desktop saved. Redirect to desktop detail page.
+        res.redirect(desktop.url);
+      }
+    }
+  }),
+];
+
 
 // Display desktop delete form on GET.
 exports.desktop_delete_get = asyncHandler(async (req, res, next) => {
